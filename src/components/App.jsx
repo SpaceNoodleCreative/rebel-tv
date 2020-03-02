4c1663de90e2be44a42e3f98d171a7c890e1bc98
Tamara Yadvichuk <tomsyadvichuks@users.noreply.github.com>
2020-03-02 22:09:07 +0100

    Add store

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,34 +6,34 @@ import "../App.scss";
 import Show from "./Show";
 import { Episode } from "./Episode";
 import { Home } from "./Home";
-// import store from "../store";
+import store from "../store";
 
 function App() {
   return (
-    // <Provider store={store}>
-    <Router>
-      <div className="App">
-        <nav className="main-nav">
-          <Link to="/">
-            <img src={logo} className="logo hvr-buzz" alt="logo" />
-          </Link>
-          <div className="home-bubble">
-            Made with{" "}
-            <span role="img" aria-label="love">
-              ❤️️
-            </span>
-          </div>
-        </nav>
-        <main>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/:showId" exact component={Show} />
-            <Route path="/:showId/:episodeId" component={Episode} />
-          </Switch>
-        </main>
-      </div>
-    </Router>
-    // </Provider>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <nav className="main-nav">
+            <Link to="/">
+              <img src={logo} className="logo hvr-buzz" alt="logo" />
+            </Link>
+            <div className="home-bubble">
+              Made with{" "}
+              <span role="img" aria-label="love">
+                ❤️️
+              </span>
+            </div>
+          </nav>
+          <main>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/:showId" exact component={Show} />
+              <Route path="/:showId/:episodeId" component={Episode} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
